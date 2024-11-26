fe466a2aaa6679dd83f5aa3c3fc30a9939897bad
Mateusz <115219630+Cent0l@users.noreply.github.com>
2024-11-26 15:26:46 +0100

    Add files via upload

--- page1.4/index.php
+++ page1.4/index.php
@@ -1,49 +1,53 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+
+if ($_GET['idp'] == '') $strona = 'html/mainpage.html';
+if ($_GET['idp'] == 'Sprzet') $strona = 'html/placeholder.html';
+if ($_GET['idp'] == 'Zastosowania') $strona = 'html/placeholder.html';
+if ($_GET['idp'] == 'Gry') $strona = 'html/gry_subpage.html';
+if ($_GET['idp'] == 'Koszta') $strona = 'html/koszta_subpage.html';
+if ($_GET['idp'] == 'Wypowiedzi') $strona = 'html/placeholder.html';
+if ($_GET['idp'] == 'Skrypty') $strona = 'html/skrypty.html';
+
+$nr_indeksu = '169226';
+$nrGrupy = 'isi1';
+?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="language" content="pl">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Mateusz Cętkowski">
+    <title>Moje hobby - Simracing</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>Moje hobby to simracing</title>
-    <script src="js/kolorujtlo.js" type="text/javascript"></script>
-    <script src="js/timedate.js" type="text/javascript"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-	
 </head>
+<body onload="startClock()">
+    <header>
+        <h1>Moje hobby - Simracing</h1>
+    </header>
 
-<?php
-error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
-if ($_GET['idp'] == '') {
-    $strona = 'html/mainpage.html';
-} elseif ($_GET['idp'] == 'main') {
-    $strona = 'html/koszta_subpage.html';
-} elseif ($_GET['idp'] == 'koszta') {
-    $strona = 'html/skrypty.html';
-} elseif ($_GET['idp'] == 'skrypty') {
-    $strona = 'html/gry_subpage.html';
-} elseif ($_GET['idp'] == 'placeholder') {
-    $strona = 'html/placeholder.html';
-} else {
-    $strona = 'html/glowna.html';
-}
+    <nav>
+        <a href="index.php?idp=Sprzet">Sprzęt</a>
+        <a href="index.php?idp=Zastosowania">Zastosowania</a>
+        <a href="index.php?idp=Gry">Gry</a>
+        <a href="index.php?idp=Koszta">Koszta</a>
+        <a href="index.php?idp=Wypowiedzi">Wypowiedzi</a>
+        <a href="index.php?idp=Skrypty">Skrypty</a>
+    </nav>
 
-if (file_exists($strona)) {
-    include($strona);
-} else {
-    echo "Nie znaleziono strony.";
-}
+    <div class="container">
+        <?php
+        if (file_exists($strona)) {
+            include($strona);
+        } else {
+            echo 'Nie działa';
+        }
+        ?>
+    </div>
 
-
-
-
-
- $nr_indeksu = '169226';
- $nrGrupy = 'isi1';
- echo 'Autor: Mateusz Cętkowski '.$nr_indeksu.' grupa '.$nrGrupy.'';
- 
-?>
-
-
-
+    <footer>
+        <p>Autor: Mateusz Cętkowski, numer indeksu: <?php echo $nr_indeksu; ?>, grupa: <?php echo $nrGrupy; ?></p>
+    </footer>
+</body>
 </html>
